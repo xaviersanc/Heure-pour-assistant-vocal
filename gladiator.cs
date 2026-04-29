8433929295a36df6ccd3e124bed809707c53d8ff
Xavier Sanchez <xavier.espagne@gmail.com>
2026-04-29 13:55:23 +0200

    Atelier 4 : Ajout de gladiator

---
+++ gladiator.cs
@@ -0,0 +1,17 @@
+public class Gladiator(string name, int health, int strength, int armor) {
+    public string Name { get; } = name;
+    public int Health { get; private set; } = health;
+    public int Strength { get; private set; } = strength;
+    public int Armor { get; private set; } = armor;
+
+ public void Attack(Gladiator opponent, Dice dice) {
+ var score = dice.Roll();
+ throw new NotImplementedException("To be done");
+ }
+}
+
+public class Dice(int faces) {
+ private readonly Random rnd = new ();
+
+ public int Roll() => rnd.Next(1, faces + 1);
+}
