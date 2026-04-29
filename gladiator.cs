cd9efc27a3cfdc0f8682977e3f1d9573d7325ade
MatheoCASSY <matheo.cassy@ecoles-epsi.net>
2026-04-29 12:44:37 +0000

    Atelier 4 - Arène TDD - Étape 1 - Préparation de Gladiator

--- gladiator.cs
+++ gladiator.cs
@@ -4,14 +4,18 @@ public class Gladiator(string name, int health, int strength, int armor) {
     public int Strength { get; private set; } = strength;
     public int Armor { get; private set; } = armor;
 
- public void Attack(Gladiator opponent, Dice dice) {
- var score = dice.Roll();
- throw new NotImplementedException("To be done");
- }
+    public void Attack(Gladiator opponent, IDice dice) {
+        var score = dice.Roll();
+        throw new NotImplementedException("To be done");
+    }
 }
 
-public class Dice(int faces) {
- private readonly Random rnd = new ();
+public interface IDice {
+    int Roll();
+}
 
- public int Roll() => rnd.Next(1, faces + 1);
+public class Dice(int faces) : IDice {
+    private readonly Random rnd = new ();
+
+    public int Roll() => rnd.Next(1, faces + 1);
 }
